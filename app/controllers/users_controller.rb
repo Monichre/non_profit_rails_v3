b9cdd1d30e47a6223787bb7fd00b032a9ba2586f
Liam Ellis <liamhellis@gmail.com>
2016-09-18 14:02:09 -0700

    implemented adding nonprofits to users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    @non_profit = NonProfit.find(params[:id])
+    #binding.pry
+    @user.non_profits.push(@non_profit)
+    redirect_to user_path(@user)
+
+  end
+
+
+
+  private
+
+
 end
